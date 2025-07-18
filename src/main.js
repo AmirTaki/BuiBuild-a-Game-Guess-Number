13db9dfa5cb2eb406edd29917b76ae4b05f1208d
amir <amirtaki623@gmail.com>
2025-07-18 22:37:46 +0330

    ranomNumber

--- src/main.js
+++ src/main.js
@@ -2,3 +2,9 @@ const input = document.querySelector("input")
 const guess = document.querySelector(".guess")
 const checkButton = document.querySelector("button")
 const chences = document.querySelector(".chances")
+
+
+input.focus();
+
+let randomNum = Math.floor(Math.random() * 100)
+console.log(randomNum)
