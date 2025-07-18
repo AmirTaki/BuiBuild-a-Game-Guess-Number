09d475a6c61fc1aed28bad8766f472bee49e14e0
amir <amirtaki623@gmail.com>
2025-07-18 22:39:25 +0330

    checkButton add Event Listener

--- src/main.js
+++ src/main.js
@@ -1,10 +1,16 @@
 const input = document.querySelector("input")
 const guess = document.querySelector(".guess")
 const checkButton = document.querySelector("button")
-const chences = document.querySelector(".chances")
+const ramainChences = document.querySelector(".chances")
 
 
 input.focus();
 
 let randomNum = Math.floor(Math.random() * 100)
-console.log(randomNum)
+chance = 10;
+
+
+checkButton.addEventListener("click", (e)=> {
+    chance--;
+    
+})
