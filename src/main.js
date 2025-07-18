d9914cdfb3a686a0e61c274588d932f81cdf4d07
amir <amirtaki623@gmail.com>
2025-07-18 22:31:49 +0330

    query selector js at project

--- src/main.js
+++ src/main.js
@@ -1,1 +1,4 @@
-s
+const input = document.querySelector("input")
+const guess = document.querySelector(".guess")
+const checkButton = document.querySelector("button")
+const chences = document.querySelector(".chances")
