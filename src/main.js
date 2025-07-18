490cb8ada83b59350355ab6fa2f9a324be19ba61
amir <amirtaki623@gmail.com>
2025-07-18 22:50:43 +0330

    guess & checkButton at project

--- src/main.js
+++ src/main.js
@@ -7,13 +7,19 @@ const ramainChences = document.querySelector(".chances")
 input.focus();
 
 let randomNum = Math.floor(Math.random() * 100)
+console.log(randomNum)
 chance = 10;
 
 
 checkButton.addEventListener("click", (e)=> {
     chance--;
     let inputValue = input.value
-    if(inputValue === randomNum){
-        [guess.textContent, checkButton.disabled] = ["Congratulations", true]
+    if(inputValue == randomNum){
+        // [guess.textContent, checkButton.disabled] = ["Congratulations", true]
+        // [checkButton.textContent, guess.style.color] = ['Replay', '#333']
+        guess.innerText = "Congratulations"
+        checkButton.disabled = true
+        checkButton.innerText = "Replay"
+        guess.style.color = "#333"
     }
 })
